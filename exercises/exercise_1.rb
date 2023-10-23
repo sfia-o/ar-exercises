ea9c2173c00391f03f0e9c26ca6062865798783a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-10-22 18:27:17 -0700

    Refactor and add store total

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,8 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
-store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
-store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: true)
+
+puts "Number of stores: #{Store.count}"
